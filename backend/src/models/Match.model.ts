2c102b28ada8c9bde42c418e217f59f61d43e9cc
Akarshit Kaushik <akarshit0206@gmail.com>
2026-07-12 22:45:41 +0530

    working on Match Schema

--- backend/src/models/Match.model.ts
+++ backend/src/models/Match.model.ts
@@ -1,12 +1,70 @@
-import mongoose, { type Int32 } from "mongoose"
+import mongoose, { model, type Int32 } from "mongoose"
 import {Schema, Model, Types, Document} from "mongoose";
 
+interface IPlayerStats{
+    player: Types.ObjectId;
+    testCasesPassed: number;
+    timeTaken: number;
+}
 interface IMatchSchema extends Document{
-    player1: Types.ObjectId;
-    player2: Types.ObjectId;
-    winner: Types.ObjectId | null;
-    score1: Int32;
-    score2: Int32;
+    player1Stats: IPlayerStats;
+    player2Stats: IPlayerStats;
+    winner?: Types.ObjectId | null;
+    totalTestCases: number;
+    duration: number;
+    endedAt?: Date | null;
     createdAt: Date;
     updatedAt: Date;
 }
+
+const playerStatsSchema: Schema<IPlayerStats>= new Schema({
+    player: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    testCasesPassed:{
+        type: Number,
+        required: true,
+        default: 0,
+    },
+    timeTaken:{
+        type: Number,
+        default: -1,
+    }
+}, {_id: false})
+
+const matchSchema: Schema<IMatchSchema>= new Schema({
+    player1Stats:{
+        type: playerStatsSchema,
+        required: true,
+    },
+    player2Stats:{
+        type: playerStatsSchema,
+        required: true,
+    },
+    winner:{
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        default: null,
+    },
+    totalTestCases:{
+        type: Number,
+        required: true,
+    },
+    duration:{
+        type: Number,
+        required: true,
+    },
+    endedAt:{
+        type: Date,
+        default: null,
+    }
+},
+    {
+        timestamps: true
+    }
+);
+const Match : Model<IMatchSchema>= model("Match", matchSchema);
+
+export default Match;

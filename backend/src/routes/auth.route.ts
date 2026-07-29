797d87da5895d189b14b06678392b9d1972806a4
Akarshit Kaushik <akarshit0206@gmail.com>
2026-07-29 22:24:11 +0530

    upadate auth routes

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -1,7 +1,10 @@
 import express from "express";
-import {registerUser} from "../controllers/auth.controller.js";
+import {registerUser, loginUser, logoutUser, getCurrentUser} from "../controllers/auth.controller.js";
 import { asyncHandler } from "../utils/index.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 export const authRouter= express.Router();
 
 authRouter.route("/register").post(asyncHandler(registerUser));
+authRouter.route("/login").post(asyncHandler(loginUser));
+authRouter.route("/logout").post(verifyJWT, asyncHandler(logoutUser));
